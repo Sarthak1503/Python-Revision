e320833da67990e1ba898a341e8a15a2c270a8af
www.sarthak1503@gmail.com <www.sarthak1503@gmail.com>
2020-12-24 18:50:35 +0530

    Syntax and basics

---
+++ Chapter 1- Module, pip and comments/Module_usage.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('H:\\Python Notes\\1. Chapter 1\\play.mp3')
---
+++ Chapter 1- Module, pip and comments/list_directory.py
@@ -0,0 +1,4 @@
+#Write a python program to print the contents of a directory using os module.
+
+import os
+print(os.listdir())
